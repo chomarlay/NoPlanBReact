00c540a79be967c2b22eef776666ee724e6d245b
chomarlay <chomarlay.aung@gmail.com>
2020-12-15 06:10:57 +1100

    Router initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react'
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import About from './components/pages/About';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-        NoPlanB
-    </div>
-  );
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/about' component={About} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
